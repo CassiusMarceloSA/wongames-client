813e7db30a8af9f55bc69d7809ca863950cbc165
Cassius Avila <cassius.avila@groundsportech.com>
2020-09-03 21:55:25 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/Main.stories.tsx
@@ -0,0 +1,12 @@
+import Main from '.';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+export default {
+  title: 'Main',
+  component: Main,
+  argTypes: {
+    color: { control: 'color' }
+  }
+} as Meta;
+
+export const Basic: Story = (args) => <Main {...args} />;
