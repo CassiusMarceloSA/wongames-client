2ca6b0363437515d12e532f98e20076d955aca8b
Cassius Avila <cassius.avila@groundsportech.com>
2020-09-19 22:51:11 -0300

    :sparkles: added ribbon component

---
+++ src/components/Ribbon/Ribbon.stories.tsx
@@ -0,0 +1,29 @@
+import Ribbon from '.';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { RibbonProps } from './Ribbon';
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon,
+  args: {
+    children: 'Best seller'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta;
+
+export const Basic: Story<RibbonProps> = (args) => (
+  <div
+    style={{
+      width: '40rem',
+      height: '25rem',
+      position: 'relative',
+      backgroundColor: '#888'
+    }}
+  >
+    <Ribbon {...args} />
+  </div>
+);
